b0d633fcc356750ab1dbd2d56326fa474e74c6b1
강란규 <bpos333@gmail.com>
2025-01-03 18:28:50 +0900

    회원가입 완료

--- src/page/Signup.tsx
+++ src/page/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import supabase from "../utils/supabase";
 
 const Signup = () => {
@@ -7,27 +7,59 @@ const Signup = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const navigate = useNavigate();
 
-const SignupHendler = async (e: React.FormEvent<HTMLFormElement>)=>  {
-  e.preventDefault();
+  const SignupHendler = async (e: React.FormEvent<HTMLFormElement>) => {
+    const emailCheck = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailCheck.test(email)) {
+      console.log("이메일을 다시입력해주세요 ");
+      alert("이메일을 다시입력해주세요 ");
+    }
+
+    if (password.length < 8) {
+      alert("비밀번호는 최소 8자리 이상이어야 합니다.");
+    }
+
+    if (password !== confirmPassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+    }
+
+    e.preventDefault();
     let { data, error } = await supabase.auth.signUp({
       email,
       password,
       options: {
         data: {
-          nickname
-        }
-      }
+          nickname,
+        },
+      },
     });
+    if (error) {
+      console.error("supabase auth error", error);
+    }
+
+    console.log(data);
+    const { error: Errordata } = await supabase
+      .from("user")
+      .insert([{ id: data.user?.id, nickname: nickname, email: email }])
+      .select();
+    if (Errordata) {
+      alert(`유저 생성에 실패했습니다. ${Errordata.message}`);
+      return;
+    }
+    navigate("/");
   };
 
-
- }
   return (
     <>
       <div className="flex justify-center  items-center flex-col h-screen">
         <h1 className=" text-2xl font-bold">회원가입 </h1>
-        <form action="" className="flex flex-col gap-4 w-80  " onSubmit={SignupHendler}>
+        <form
+          action=""
+          className="flex flex-col gap-4 w-80  "
+          onSubmit={SignupHendler}
+        >
           <div className=" flex flex-col gap-2">
             <label className="text-sm" htmlFor="">
               이메일
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../type/database.types";
+
+
+
 const supabaseUrl = "https://kdszztcbvfcmmyihescu.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
